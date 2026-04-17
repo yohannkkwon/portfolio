522283a12e8e7f0f05c1833eaa98d09f2a163503
Yohan Kwon <142409009+yohannkkwon@users.noreply.github.com>
2026-04-17 16:04:10 -0700

    Lab 3 - Step 2

--- global.js
+++ global.js
@@ -3,3 +3,11 @@ console.log('IT\'S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+currentLink?.classList.add('current');
